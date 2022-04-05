bc53b796b9a229eea8aefc92515da50eb560d6ab
Gully <gullydsm@aol.com>
2022-04-04 20:10:53 -0500

    Added my name paragraph to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
+            <p>Jeff Gullion</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
