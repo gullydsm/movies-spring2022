c78009b3102ac5f16d290a89e6be9f367ecd8959
Gully <gullydsm@aol.com>
2022-04-04 20:54:39 -0500

    Displaying movie titles using map()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ const App = () => {
     return (
         <div>
             <p>Jeff Gullion</p>
+            <p>Hello</p>
+
+            {/* the following .map is like outputting multiple lines manually */}
+            {
+                movies.map( (movie) => <p>{movie.title}</p>)
+            }
+
+
+            <h3>Manual outputs</h3>
+            <p>{movies[0].title}</p>
+            <p>{movies[1].title}</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
